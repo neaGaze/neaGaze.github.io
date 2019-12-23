c8c5e6ab981fb1650b4bf62b177aa1aefae07443
Nigesh Shakya <nizeshshakya@gmail.com>
2019-12-23 14:54:43 -0600

    changes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 git commit -m "$msg"
 
