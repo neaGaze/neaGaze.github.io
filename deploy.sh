873d0e1ddb90c72697fa677eaf9b7f0afe0c2408
Nigesh Shakya <nizeshshakya@gmail.com>
2019-12-23 14:37:50 -0600

    Added the correct url to the hugo project

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# If a command fails then the deploy stops
+set -e
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-theme-cleanwhite # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
